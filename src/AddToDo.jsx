98ee67616efd6dc3db4a1d96fd45166ce7f4b571
Your Name <you@example.com>
2024-01-30 17:45:22 +0900

    마지막

--- src/AddToDo.jsx
+++ src/AddToDo.jsx
@@ -6,17 +6,50 @@ import { TextField, Paper, Button, Grid } from "@material-ui/core"
 class AddToDo extends React.Component{
   constructor(props){
     super(props);
+
+    //App.js 파일에서 넘겨준 메서드를 add에 저장
+    this.add = props.add;
+
+    //입력한 문자열을 저장하기 위한 속성을 생성
+    this.state = {item:{title:""}}
   }
+
+  //TextField에서 값을 입력할 때마다 호출되는 이벤트 리스너
+  onInputChange = (e) => {
+    //state.title에 입력한 내용을 대입
+    //state 수정 불가능 > cosnt thisItem = this.state.item
+    const thisItem = this.state.item
+    thisItem.title = e.target.value;
+    this.setState({item:thisItem});
+    console.log(this.state.item.title)
+  }
+
+    //+버튼을 눌렀을 때 호출되는 이벤트 리스너
+    onButtonClick = (e) => {
+      //배열에 데이터를 삽입하는 함수를 호출
+      this.add(this.state.item)
+      //텍스트 필드를 초기화
+      this.setState({item:{title:""}})
+    }
+
+    //Enter를 누르면 호출되는 이벤트 리스너
+    enterKeyEventHandler = (e) => {
+      if(e.key === "Enter"){
+        this.onButtonClick();
+      }
+    }
 
   render(){
     return(
       <Paper style={{margin:16, padding:16}}>
         <Grid container>
           <Grid xs = {7} md = {7} item style={{paddingRight:16}}>
-            <TextField placeholder="여기에 작성하시오!!!" fullWidth />
+            <TextField placeholder="여기에 작성하시오!!!" fullWidth
+            onChange={this.onInputChange} value={this.state.item.title}
+            onKeyPress={this.enterKeyEventHandler}/>
           </Grid>
           <Grid xs = {5} md = {5} item>
-            <Button fullwidth color="secondary">
+            <Button fullwidth color="secondary" onClick={this.onButtonClick}>
               +
             </Button>
           </Grid>
--- src/App.js
+++ src/App.js
@@ -17,6 +17,25 @@ class App extends React.Component {
                         {id:1, title:"vue", done:false},
                         {id:2, title:"angular", done:false}]}
   }
+
+  //데이터를 추가하는 함수
+  add = (item) => {
+    //react는 props는 수정할 수 없고
+    //state는 수정이 가능하지만 원본을 직접 수정하는 것은 안되고 
+    //setState 메서드를 이용해서만 수정이 가능합니다.
+
+    //배열에 데이터를 추가하기 위해서 state의 배열을 복사
+    const thisItems = this.state.items;
+
+    //item의 id와 done 값을 설정 - title 만 입력하기 때문
+    item.id = "ID-" + thisItems.length;
+    item.done = false;
+
+    //복사한 데이터에 데이터를 추가
+    thisItems.push(item)
+    //복사한 데이터를 다시 state에 적용
+    this.setState({items:thisItems});
+  }
   render(){
     //리턴하기 전 배열을 순회하면서 출력할 내용을 생성 , map(data,index)
     //item은 배열을 순회할 때 각각의 데이터이고 idx는 인덱스
@@ -27,22 +46,19 @@ class App extends React.Component {
     let display = this.state.items.length > 0 && (
       <Paper style = {{margin:16}}>
     {/* //idx 겹치지 않게 */}
-        <list>
+        <List>
           {this.state.items.map((item,idx) => (
             <ToDo_component item = {item} key = {idx}/>
           ))}
-        </list>
+        </List>
       </Paper>
     );
     return(
     <div className='App'>
       <Container maxWidth = "md">
-        <AddToDo />
-      </Container>
-      <Container>
+        <AddToDo add={this.add}/>
         {display}
       </Container>
-
     </div>
     )
   }
